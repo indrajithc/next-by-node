3fca949737326228e183d6224b89a2f3a3f6d4ce
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-29 00:45:25 +0530

    doc added for future use

--- src/common/base/Prepare.ts
+++ src/common/base/Prepare.ts
@@ -14,6 +14,7 @@ import { exit } from "process";
 
 const prepareConfiguration = () => {
   const configJson = JSON.stringify(config);
+  /** Update this json for dynamic config on run time */
   fs.writeFileSync(path.join(config.rootDir, systemConstants.CONFIG_JSON), configJson);
 };
 
