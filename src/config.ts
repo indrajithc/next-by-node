05a1b2350233ab54f40b5c3fe45f957edbf263ca
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-28 21:36:12 +0530

    basic config const added

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,13 @@
  * Basic config
  */
 
-const outputDir: string = process.env.OUTPUT_DIR || "../output";
+const outputDir: string = process.env.OUTPUT_DIR || "./output";
 
 const rootDir: string = __dirname;
 
+export const systemConstants = {
+  CONFIG_JSON: "runtime.config.json",
+};
 export default {
   outputDir,
   rootDir,
