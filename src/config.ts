41e598b99796ef34f0b1b436d58e06760f99dd30
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-28 21:06:28 +0530

    config file

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+/**
+ * Basic config
+ */
+
+const outputDir: string = process.env.OUTPUT_DIR || "../output";
+
+const rootDir: string = __dirname;
+
+export default {
+  outputDir,
+  rootDir,
+};
