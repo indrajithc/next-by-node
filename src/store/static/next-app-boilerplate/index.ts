0b382bb86e26ac77673abe8a8aa804ee859c3b43
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-30 16:39:56 +0530

    page content added

---
+++ src/store/static/next-app-boilerplate/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Default config
+ */
+
+import * as PAGE_01 from "@store/static/next-app-boilerplate/data/src/app/page";
+
+const configuration = require("@store/static/next-app-boilerplate/configuration.json");
+
+export default {
+  configuration,
+  dynamicModules: {
+    PAGE_01,
+  },
+};
