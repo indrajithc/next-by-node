2de364aa6f3782248ccb2d59013a806b3d201619
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-31 11:49:01 +0530

    sample base

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-
+// import File from "@base/File";
+// import Directory from "@base/Directory";
+import Folder from "@base/Folder";
 
 const main = () => {
+  console.info("Main called");
 
-console.log("Main page")
-}
+  // const sampleFile = new File("./test.txt");
+
+  // const sampleDirectory = new Directory("./app");
+
+  // console.log(sampleFile);
+  // console.log({ sampleDirectory });
+
+  const staticData = require("@store/static/next-app-boilerplate");
+
+  const dynamicData = require("@store/dynamic/details/next-app-boilerplate.json");
+
+  new Folder(staticData.default, { assetPath: "store/static", dynamicData });
+};
 
 export default main;
